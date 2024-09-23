177bb7ba1a05398e996e879870bd94849db1e575
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-23 15:27:29 -0300

    Feat: modificação para que também sejam mostrados quantos alunos tem nota abaixo de 6,0 e quantos tem notas acima de 8.

--- Desafios23Setembro/NotasTurma.java
+++ Desafios23Setembro/NotasTurma.java
@@ -5,22 +5,28 @@ public class NotasTurma {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Pedir ao usuário o número de alunos
+
         System.out.print("Digite o número de alunos na turma: ");
         int numAlunos = scanner.nextInt();
+        scanner.nextLine();
 
-        // Criar um vetor para armazenar as notas com o tamanho baseado no número de alunos
+
+        String[] nomes = new String[numAlunos];
         double[] notas = new double[numAlunos];
-
 
         double soma = 0;
         double maiorNota = 0;
         double menorNota = 0;
+        int countAbaixo6 = 0;
+        int countAcima8 = 0;
 
 
         for (int i = 0; i < numAlunos; i++) {
-            System.out.print("Digite a nota do aluno " + (i +1) + ": ");
+            System.out.print("Digite o nome do aluno " + (i + 1) + ": ");
+            nomes[i] = scanner.nextLine();
+            System.out.print("Digite a nota do aluno " + nomes[i] + ": ");
             notas[i] = scanner.nextDouble();
+            scanner.nextLine();
             soma += notas[i];
 
 
@@ -35,14 +41,29 @@ public class NotasTurma {
                     menorNota = notas[i];
                 }
             }
+
+            if (notas[i] < 6.0) {
+                countAbaixo6++;
+            }
+            if (notas[i] > 8.0) {
+                countAcima8++;
+            }
         }
 
 
         double media = soma / numAlunos;
 
 
+        System.out.println("\nNotas dos alunos:");
+        for (int i = 0; i < numAlunos; i++) {
+            System.out.println("Aluno: " + nomes[i] + " - Nota: " + notas[i]);
+        }
+
+
         System.out.println("\nMédia geral da turma: " + media);
         System.out.println("Maior nota: " + maiorNota);
         System.out.println("Menor nota: " + menorNota);
+        System.out.println("Número de alunos com nota abaixo de 6,0: " + countAbaixo6);
+        System.out.println("Número de alunos com nota acima de 8,0: " + countAcima8);
     }
 }
